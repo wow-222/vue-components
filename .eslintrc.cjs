d37c6a503d9da3f9d8edebb46cb6d5bf5bea4ad6
wuao <ww100862681@163.com>
2024-05-15 23:14:07 +0800

    add code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,20 @@
 module.exports = {
-	root: true,
-	'extends': [
-		'plugin:vue/vue3-essential',
-		'eslint:recommended',
-		'@vue/eslint-config-typescript',
-		"plugin:prettier/recommended"
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module'
-	},
-	rules: {
-		indent: ['error', 2, {
-			SwitchCase: 1
-		}],
-		'linebreak-style': ['error', 'windows'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'never']
-	}
+  root: true,
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1
+      }
+    ],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'vue/multi-word-component-names': 0
+  }
 }
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ButtonProps } from './type'
+
+defineProps<ButtonProps>()
+</script>
+
+<template>
+  <button></button>
+</template>
+
+<style scoped></style>
